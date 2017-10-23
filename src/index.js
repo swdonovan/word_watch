dfbf2fe80c820dfc2670b4fd8f76410c81a85acc
Wil Donovan <wil.donovan@gmail.com>
2017-10-23 09:50:02 -0600

    appending the top word finally after adding jquery to the index file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 const $ = require('jquery')
 const Word = require('../lib/model/words')
 const $topWord = $('.top-word')
+const $textSubmission = $('.text-submission')
 
 document.addEventListener("DOMContentLoaded", () => {
   Word.topWord().then((data) => {
+    $topWord.append('<h4> Top Word: ' +
+      Object.keys(data.word)[0] +
+      " (" +
+      data.word[Object.keys(data.word)] +
+      ")" +
+      '</h4>'
+    )
+  })
+
+  $($textSubmission).on('click', 'button', () => {
     debugger;
-    $topWord.append('<h4> Top Word:' + Object.keys(data.word)[0] + '</h4>')
   })
 })
