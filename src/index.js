b8769ae160310b8050f4e7df1933ceb8fe34128f
Wil Donovan <wil.donovan@gmail.com>
2017-10-26 07:49:21 -0600

    clean up

--- src/index.js
+++ src/index.js
@@ -1,8 +1,4 @@
 const $ = require('jquery')
-const Word = require('../lib/model/words')
-const $topWord = $('.top-word')
-const $wordCount = $('.word-count')
-const HtmlRunner = require('../lib/helper/HtmlRunner')
 const WordRunner = require('../lib/WordsRunner')
 
 document.addEventListener("DOMContentLoaded", () => {
